02027b1dfcc53dda4f3ec252bd9aa87a8ecfde4c
Bss <mx.bss@proton.me>
2025-05-27 17:47:27 +0200

    use snacks to display images

--- lua/bss/plugins/snacks.lua
+++ lua/bss/plugins/snacks.lua
@@ -8,8 +8,14 @@ return {
 
 		---@type snacks.Config
 		opts = {
-
 			bigfile = { enabled = true },
+			image = {
+				doc = {
+					max_width = 30,
+					max_height = 10,
+					inline = false,
+				},
+			},
 
 			dashboard = {
 				enabled = true,
