be3a2526c713e8022dfe20c73d4a57d44a275274
Bss <mx.bss@proton.me>
2025-05-30 03:18:20 +0200

    show icons

--- lua/bss/plugins/oil.lua
+++ lua/bss/plugins/oil.lua
@@ -1,11 +1,18 @@
 return {
-  'stevearc/oil.nvim',
-  enabled = true,
-  lazy = false,
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	"stevearc/oil.nvim",
+	enabled = true,
+	lazy = false,
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		columns = {
+			"icon",
+			-- "permissions",
+			-- "size",
+			-- "mtime",
+		},
+	},
+	-- Optional dependencies
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
