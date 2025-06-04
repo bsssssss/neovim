d409ea3560c75595cb19040f390dfc2adc40fb20
Bss <mx.bss@proton.me>
2025-06-04 13:06:22 +0200

    disable kitty nav

--- lua/bss/plugins/kitty.lua
+++ lua/bss/plugins/kitty.lua
@@ -13,14 +13,14 @@ return {
 			})
 		end,
 	},
-	{
-		"knubie/vim-kitty-navigator",
-		config = function()
-			vim.g.kitty_navigator_no_mappings = 1
-			vim.keymap.set({ "n", "i" }, "<C-`>h", "KittyNavigateLeft<CR>")
-			vim.keymap.set({ "n", "i" }, "<C-`>j", "KittyNavigateDown<CR>")
-			vim.keymap.set({ "n", "i" }, "<C-`>k", "KittyNavigateUp<CR>")
-			vim.keymap.set({ "n", "i" }, "<C-`>l", "KittyNavigateRight<CR>")
-		end,
-	},
+	-- {
+	-- 	"knubie/vim-kitty-navigator",
+	-- 	config = function()
+	-- 		vim.g.kitty_navigator_no_mappings = 1
+	-- 		vim.keymap.set({ "n", "i" }, "<C-`>h", "KittyNavigateLeft<CR>")
+	-- 		vim.keymap.set({ "n", "i" }, "<C-`>j", "KittyNavigateDown<CR>")
+	-- 		vim.keymap.set({ "n", "i" }, "<C-`>k", "KittyNavigateUp<CR>")
+	-- 		vim.keymap.set({ "n", "i" }, "<C-`>l", "KittyNavigateRight<CR>")
+	-- 	end,
+	-- },
 }
