8e76eb1a1f0f1c0cd90d5f15d7dfb80a288eff00
Bss <mx.bss@proton.me>
2025-06-08 13:16:01 +0200

    add mini.hipatterns (highlight hex colors)

--- lua/bss/plugins/mini.lua
+++ lua/bss/plugins/mini.lua
@@ -6,6 +6,13 @@ return {
 			require("mini.surround").setup()
 			require("mini.pairs").setup()
 			require("mini.align").setup()
+
+            local hipatterns = require("mini.hipatterns")
+            hipatterns.setup({
+                highlighters = {
+                    hex_color = hipatterns.gen_highlighter.hex_color(),
+                }
+            })
 		end,
 	},
 }
