3107c9fc2ad45f9c8ea8e0ca96d64535daeddff8
Bss <mx.bss@proton.me>
2025-06-11 02:43:53 +0200

    add kanagawa lualine theme

---
+++ lua/bss/themes/lualine/kanagawa.lua
@@ -0,0 +1,54 @@
+local colors = require("kanagawa.colors").setup()
+local palette_colors = colors.palette
+local theme_colors = colors.theme
+
+local palette = {
+	bg = theme_colors.ui.bg,
+	primary = palette_colors.dragonPink,
+	secondary = palette_colors.fujiGray,
+	text = palette_colors.fujiWhite,
+}
+
+local theme = {
+	normal = {
+		a = { bg = palette.bg, fg = palette.primary, gui = "bold" },
+		b = { bg = palette.bg, fg = palette.secondary },
+		c = { bg = palette.bg, fg = palette.secondary },
+
+		x = { bg = palette.bg, fg = palette.secondary },
+		y = { bg = palette.bg, fg = palette.secondary },
+		z = { bg = palette.bg, fg = palette.primary },
+	},
+	insert = {
+		a = { bg = palette.primary, fg = palette.bg, gui = "bold" },
+		b = { bg = palette.bg, fg = palette.secondary },
+		c = { bg = palette.bg, fg = palette.secondary },
+
+		x = { bg = palette.bg, fg = palette.secondary },
+		y = { bg = palette.bg, fg = palette.secondary },
+		z = { bg = palette.bg, fg = palette.primary },
+	},
+	visual = {
+		a = { bg = palette.primary, fg = palette.bg, gui = "bold" },
+		b = { bg = palette.bg, fg = palette.secondary },
+		c = { bg = palette.bg, fg = palette.secondary },
+
+		x = { bg = palette.bg, fg = palette.secondary },
+		y = { bg = palette.bg, fg = palette.secondary },
+		z = { bg = palette.bg, fg = palette.primary },
+	},
+	replace = {
+		a = { bg = palette.primary, fg = palette.bg, gui = "bold" },
+		b = { bg = palette.bg, fg = palette.secondary },
+		c = { bg = palette.bg, fg = palette.secondary },
+
+		x = { bg = palette.bg, fg = palette.secondary },
+		y = { bg = palette.bg, fg = palette.secondary },
+		z = { bg = palette.bg, fg = palette.primary },
+	},
+}
+
+return {
+	palette = palette,
+	theme = theme,
+}
