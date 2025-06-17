30f08942b49fc2dd0377e4a311c2f8d77dd2d06d
Bss <mx.bss@proton.me>
2025-06-17 15:52:12 +0200

    enabled

--- lua/bss/plugins/lualine.lua
+++ lua/bss/plugins/lualine.lua
@@ -1,6 +1,7 @@
 ---@diagnostic disable: undefined-field
 return {
 	"nvim-lualine/lualine.nvim",
+  enabled = true,
 	config = function()
 		local colors = require("bss.themes.lualine.kanagawa")
 		local theme = colors.theme
