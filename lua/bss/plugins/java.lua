0eaee8da98ff2f0d437aac8caf904343a9ab4a72
Bss <mx.bss@proton.me>
2025-06-19 08:20:08 +0200

    remove commented

--- lua/bss/plugins/java.lua
+++ lua/bss/plugins/java.lua
@@ -1,12 +1,4 @@
 return {
-	-- {
-	-- 	"nvim-java/nvim-java",
-	-- 	dependencies = {
-	-- 		"mfussenegger/nvim-dap",
-	-- 		"rcarriga/nvim-dap-ui",
-	-- 		"nvim-neotest/nvim-nio", -- Dépendance importante
-	-- 	},
-	-- },
 	{
 		"mfussenegger/nvim-jdtls",
 		ft = "java",
