6f3db4d8a59dc92d85b84dbe1247fd7db775dcd5
Bss <mx.bss@proton.me>
2025-06-24 20:25:58 +0200

    add back html to tabwidth 2 command

--- lua/bss/core/autocmds.lua
+++ lua/bss/core/autocmds.lua
@@ -41,7 +41,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "lua", "html", "json" },
+	pattern = { "lua", "json", "html" },
 	callback = function()
 		local bufnr = vim.api.nvim_get_current_buf()
 		vim.bo[bufnr].tabstop = 2
