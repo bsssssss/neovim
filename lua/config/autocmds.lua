d28b3c1f8ca43be655d6a1d11d8971221110e9ab
Bss <mx.bss@proton.me>
2025-06-25 16:04:51 +0200

    format

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -22,6 +22,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local client = vim.lsp.get_client_by_id(event.data.client_id)
 		if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
 			local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+
 			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 				buffer = event.buf,
 				group = highlight_augroup,
