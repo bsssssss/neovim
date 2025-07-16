a5882c85ab7d3bcb8f222fad31afcefa4ab20c38
Bss <mx.bss@proton.me>
2025-07-16 04:11:46 +0200

    try cmd

--- lsp/supercollider.lua
+++ lsp/supercollider.lua
@@ -4,10 +4,14 @@ return {
 		"--log-file",
 		"/tmp/sc_lsp_output.log",
 		"--verbose",
+		-- "--receive-port",
+		-- "57301",
+		-- "--send-port",
+		-- "57300",
 		"--", -- indicates the args that follow are to be passed to sclang
 		"-u",
-		-- "57300", -- e.g. custom UDP listening port for sclang
-		"57110", -- e.g. custom UDP listening port for sclang
+		"57300", -- e.g. custom UDP listening port for sclang
+		-- "57110", -- e.g. custom UDP listening port for sclang
 		-- "-l",
 		-- "/Users/bss/Library/Application Support/SuperCollider/sclang_conf.yaml", -- e.g. full path to config file
 	},
