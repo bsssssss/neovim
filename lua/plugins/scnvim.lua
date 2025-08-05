5035c1cff11cb142415bdc9eb4aec04ed2f74fa5
Bss <mx.bss@proton.me>
2025-08-05 18:47:02 +0200

    use dev branch

--- lua/plugins/scnvim.lua
+++ lua/plugins/scnvim.lua
@@ -2,8 +2,10 @@ return {
 	{
 		"bsssssss/scnvim",
 		-- dir = "/Users/bss/dev/github.com/bsssssss/scnvim",
+        -- dev = false,
 		-- branch = "topic/clean-annotations",
-		branch = "feature/sclang-lsp",
+		-- branch = "feature/sclang-lsp",
+		branch = "dev",
 		dependencies = { "davidgranstrom/telescope-scdoc.nvim" },
 		event = { "FileType supercollider" },
 		config = function()
@@ -13,17 +15,20 @@ return {
 
 			-- Setup scnvim
 			scnvim.setup({
+                sclang = {
+                    startup_file = "/Users/bss/supercollider/boot.scd"
+                },
 				keymaps = {
-					["<C-E>"] = map("editor.send_line", { "i", "n" }),
+					["<C-S-E>"] = map("editor.send_line", { "i", "n" }),
 					["<C-e>"] = {
-						map("editor.send_block", { "n" }),
+						map("editor.send_block", { "n", "i" }),
 						map("editor.send_selection", "x"),
 					},
 					["<CR>"] = map("postwin.toggle"),
 					["<C-CR>"] = map("postwin.toggle", "i"),
-					["<C-x>"] = map("postwin.clear", { "n", "i" }),
+					["<C-S-X>"] = map("postwin.clear", { "n", "i" }),
 					-- ["<C-k>"] = map("signature.show", { "n", "i" }),
-					-- ["<F12>"] = map("sclang.hard_stop", { "n", "x", "i" }),
+					["<C-.>"] = map("sclang.hard_stop", { "n", "x", "i" }),
 					-- ["<leader>st"] = map("sclang.start"),
 					-- ["<leader>sk"] = map("sclang.recompile"),
 					-- ["<F1>"] = map_expr("s.boot"),
@@ -44,11 +49,11 @@ return {
 					auto_toggle_error = true,
 					horizontal = false,
 					direction = "right",
-					size = math.floor(vim.o.columns * (2/5)),
+					size = math.floor(vim.o.columns * 0.4),
 				},
 				documentation = {
 					cmd = "/opt/homebrew/bin/pandoc",
-                    direction = "right"
+					direction = "right",
 				},
 			})
 
