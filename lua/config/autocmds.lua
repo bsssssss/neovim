f7360768daf0da74ee27cd967936d6c24d31fb85
Bss <mx.bss@proton.me>
2025-08-05 18:45:16 +0200

    move lsp configs

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -118,37 +118,37 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- Tidal language server
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "tidal",
-	callback = function()
-		-- local logfile = "/tmp/tidal_ls.log"
-		local client_id = vim.lsp.start({
-			name = "tidal_ls",
-			cmd = { vim.fn.expand("~/.local/bin/tidal-ls") },
-			handlers = {
-				["window/showMessage"] = function(_, result, ctx)
-					vim.notify(
-						"LSP Message: " .. result.message,
-						vim.log.levels.INFO,
-						{ title = "Tidal LSP" }
-					)
-				end,
-				["window/logMessage"] = function(_, result, ctx)
-					vim.notify(
-						"LSP Log: " .. result.message,
-						vim.log.levels.DEBUG,
-						{ title = "Tidal LSP Log" }
-					)
-				end,
-			},
-		})
-		if not client_id then
-			vim.notify("Failed to start LSP client", vim.log.levels.ERROR)
-		else
-			vim.notify("LSP client started", vim.log.levels.INFO)
-		end
-	end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = "tidal",
+-- 	callback = function()
+-- 		-- local logfile = "/tmp/tidal_ls.log"
+-- 		local client_id = vim.lsp.start({
+-- 			name = "tidal_ls",
+-- 			cmd = { vim.fn.expand("~/.local/bin/tidal-ls") },
+-- 			handlers = {
+-- 				["window/showMessage"] = function(_, result, ctx)
+-- 					vim.notify(
+-- 						"LSP Message: " .. result.message,
+-- 						vim.log.levels.INFO,
+-- 						{ title = "Tidal LSP" }
+-- 					)
+-- 				end,
+-- 				["window/logMessage"] = function(_, result, ctx)
+-- 					vim.notify(
+-- 						"LSP Log: " .. result.message,
+-- 						vim.log.levels.DEBUG,
+-- 						{ title = "Tidal LSP Log" }
+-- 					)
+-- 				end,
+-- 			},
+-- 		})
+-- 		if not client_id then
+-- 			vim.notify("Failed to start LSP client", vim.log.levels.ERROR)
+-- 		else
+-- 			vim.notify("LSP client started", vim.log.levels.INFO)
+-- 		end
+-- 	end,
+-- })
 
 --------------------------------------------------------------------------------
 -- OTHER --
