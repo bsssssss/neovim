959b902d0c058c61d5c12618e4fb674632fab644
Bss <mx.bss@proton.me>
2025-12-01 15:52:41 +0100

    conceal 0

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -106,6 +106,7 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.softtabstop = 2
         vim.opt_local.shiftwidth = 2
         vim.opt_local.expandtab = true
+        vim.opt_local.conceallevel = 0
     end,
 })
 
