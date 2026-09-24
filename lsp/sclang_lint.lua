4da44b637e5703a12b8aa8d7c2d693c860c87b55
Bss <mx.bss@proton.me>
2026-09-24 14:37:39 +0200

    cleanup runtime lsp configs

---
+++ lsp/sclang_lint.lua
@@ -0,0 +1,9 @@
+return {
+    -- from https://github.com/lucdoebereiner/sclang-lint
+    cmd = { "sclang-lint-server" },
+    filetypes = { "supercollider" },
+    -- root_dir = function(fname)
+    --     return "/"
+    -- end,
+    settings = {},
+}
